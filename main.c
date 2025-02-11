8b337faec687fbe6d71cd827db9d10eb7d5191a5
HernandezDev <128203138+HernandezDev@users.noreply.github.com>
2025-02-11 03:10:17 -0300

    update gui

--- main.c
+++ main.c
@@ -12,11 +12,12 @@ int main() {
     // Variables para el control de pestañas
     int currentTab = 0;
     const char *tabNames = "Carga de artículos;Consulta por código;Listado Completo";
-
+    char InputNombreArticulo[50] = {0};
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
-
+    while (!WindowShouldClose()) 
+    {
+        
         // Aca ba la logica de la actualización antes de dibujar 
         //
         //
@@ -28,7 +29,9 @@ int main() {
 
         // Contenido de cada pestaña
         if (currentTab == 0) {
-            GuiGroupBox((Rectangle){10, 60, 200, 80},"Artículo");
+            GuiGroupBox((Rectangle){10, 60, 415, 80},"Artículo");
+            GuiLabel((Rectangle){50, 80, 200, 40},"Descripción");
+            GuiTextBox((Rectangle){210, 80, 200, 40},InputNombreArticulo, 50,false);
         } else if (currentTab == 1) {
             GuiGroupBox((Rectangle){10, 60, 200, 80},"Artículo");
         } else if (currentTab == 2) {
