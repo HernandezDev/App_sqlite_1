4b330ff07aa596eaa47e60a8feabcaaa54e31146
HernandezDev <128203138+HernandezDev@users.noreply.github.com>
2025-02-12 19:25:29 -0300

    update gui

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 struct Articulo
 {
     int Id;
-    char Descrp[50];
+    char IdStr[50];
+    char Nombre[50];
     float Precio;
     char PrecioStr[50];
 };
@@ -18,14 +19,15 @@ int main() {
 
     // Variables para el control de pestañas
     int currentTab = 0;
-    const char *tabNames = "Carga de artículos;Consulta por código;Listado Completo";
+    const char *tabNames = "Carga de artículos;Consulta por Id;Listado Completo";
 
     // Declarar struts para trasladar datos  (input) (consulta)
-    struct Articulo input = {0, "", 0.0f, ""};
-    struct Articulo consulta = {0, "", 0.0f, ""};
+    struct Articulo input = {0,"","", 0.0f, ""};
+    struct Articulo consulta = {0,"","", 0.0f, ""};
 
     bool EditInputNombreArticulo = false;
     bool EditInputPrecioArticulo = false;
+    bool EditConsultaId = false;
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) 
@@ -40,17 +42,26 @@ int main() {
             {
                 EditInputNombreArticulo = true;
                 EditInputPrecioArticulo = false;
+                EditConsultaId = false;
             }
             // Verificar si se hizo clic en el campo de texto del precio del artículo
             else if (CheckCollisionPointRec(mousePoint, (Rectangle){90, 130, 200, 40}) && currentTab == 0)
             {
                 EditInputNombreArticulo = false;
                 EditInputPrecioArticulo = true;
+                EditConsultaId = false;
             } 
+            else if (CheckCollisionPointRec(mousePoint, (Rectangle){90, 80, 200, 40}) && currentTab == 1)
+            {
+                EditInputNombreArticulo = false;
+                EditInputPrecioArticulo = false;
+                EditConsultaId = true;
+            }
             else 
             {
                 EditInputNombreArticulo = false;
                 EditInputPrecioArticulo = false;
+                EditConsultaId = false;
             }
         }
 
@@ -63,12 +74,16 @@ int main() {
         // Contenido de cada pestaña
         if (currentTab == 0) 
         {
+            
             GuiGroupBox((Rectangle){10, 60, 300, 200}, "Artículo");
-            GuiLabel((Rectangle){25, 80, 200, 40}, "Descripción");
-            GuiTextBox((Rectangle){90, 80, 200, 40}, input.Descrp, 50, EditInputNombreArticulo);
+            //nombre
+            GuiLabel((Rectangle){25, 80, 200, 40}, "Nombre");
+            GuiTextBox((Rectangle){90, 80, 200, 40}, input.Nombre, 50, EditInputNombreArticulo);
 
-            // Usar GuiValueBoxFloat para manejar valores flotantes
-            GuiValueBoxFloat((Rectangle){90, 130, 200, 40}, "Precio ", input.PrecioStr, &input.Precio, EditInputPrecioArticulo);
+            // precio
+            GuiLabel((Rectangle){25, 130, 200, 40}, "Precio");
+            GuiValueBoxFloat((Rectangle){90, 130, 200, 40}, "", input.PrecioStr, &input.Precio, EditInputPrecioArticulo);
+            //confirmar
             if (GuiButton((Rectangle){90, 180, 200, 40}, "Confirmar")) 
             {
                 // Aquí puedes agregar la lógica para manejar el botón "Confirmar"
@@ -77,11 +92,27 @@ int main() {
         }
         else if (currentTab == 1) 
         {
-            GuiGroupBox((Rectangle){10, 60, 200, 80}, "Artículo");
+            GuiGroupBox((Rectangle){10, 60, 300, 240}, "Artículo");
+            //Id
+            GuiLabel((Rectangle){25, 80, 200, 40}, "Id");
+            GuiValueBox((Rectangle){90, 80, 200, 40}, consulta.IdStr,&consulta.Id,0,10000000,EditConsultaId);
+            //nombre
+            GuiLabel((Rectangle){25, 130, 200, 40}, "Nombre");
+            GuiTextBox((Rectangle){90, 130, 200, 40}, consulta.Nombre, 50, false);
+            //precio
+            GuiLabel((Rectangle){25, 180, 200, 40}, "Precio");
+            GuiValueBoxFloat((Rectangle){90, 180, 200, 40}, "", consulta.PrecioStr, &consulta.Precio, false);
+            //Consultar
+            if (GuiButton((Rectangle){90, 230, 200, 40}, "Consultar")) 
+            {
+                // Aquí puedes agregar la lógica para manejar el botón "Confirmar"
+                
+            }
         }
         else if (currentTab == 2) 
         {
-            GuiGroupBox((Rectangle){10, 60, 200, 80}, "Listado");
+            GuiGroupBox((Rectangle){10, 60, 605, 330}, "Listado");
+            
         }
 
         EndDrawing();
